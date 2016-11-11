b70274b40884fefd873fc152d40caedca554f0ab
leanl17 <codigomementomori@gmail.com>
2016-11-10 21:49:44 -0300

    asd

--- argentinafilms/src/main/java/presentation/ServletRegistrarResenia.java
+++ argentinafilms/src/main/java/presentation/ServletRegistrarResenia.java
@@ -54,7 +54,7 @@ public class ServletRegistrarResenia extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	   //
 		doGet(request, response);
 
 		Peliculas pelicula = (Peliculas) request.getSession().getAttribute("selectPelicula");
